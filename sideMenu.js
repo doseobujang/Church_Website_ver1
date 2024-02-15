f66193c9b086d04583bb738a637334aff8deba0b
doseobujang <doseobujang@naver.com>
2024-02-16 03:13:06 +0900

    2

--- sideMenu.js
+++ sideMenu.js
@@ -1,5 +1,6 @@
 var Show = {
     sideMenu:function() {
         document.querySelector(".side-menu").style.display = "block";
+        document.querySelector("#content").style.display = "none"
     }
 }
