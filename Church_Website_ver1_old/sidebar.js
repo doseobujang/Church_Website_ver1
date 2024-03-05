b22ebc1be5ce46670f4cdd223e3bfe52fecdbad9
doseobujang <doseobujang@naver.com>
2024-03-05 12:25:31 +0900

    edit

--- Church_Website_ver1_old/sidebar.js
+++ Church_Website_ver1_old/sidebar.js
@@ -1,21 +1,21 @@
 var Show = {
-    Sidebar:function() {
-      var computedStyle = window.getComputedStyle(sidebar);
-        if (computedStyle.right === '-250px') {
-          sidebar.style.right = '0';
-        } else {
-          sidebar.style.right = '-250px';
-        }
-      this.Opaquely();
-    },
-  
-    Opaquely:function() {
-      var opacityStyle = document.querySelector('#opacity').style;
-      if(opacityStyle.display === 'none') {
-        opacityStyle.display = 'block';
+  Sidebar:function() {
+    var computedStyle = window.getComputedStyle(sidebar);
+      if (computedStyle.right === '-250px') {
+        sidebar.style.right = '0';
+      } else {
+        sidebar.style.right = '-250px';
       }
-      else {
-        opacityStyle.display = 'none';
-      }
+    this.Opaquely();
+  },
+
+  Opaquely:function() {
+    var opacityStyle = document.querySelector('#opacity').style;
+    if(opacityStyle.display === 'none') {
+      opacityStyle.display = 'block';
+    }
+    else {
+      opacityStyle.display = 'none';
     }
   }
+}
